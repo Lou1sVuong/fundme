4685a4727df104e8201991697ec21548fcdd1d2d
Lou1sVuong <lou1svuong.dev@gmail.com>
2025-03-26 05:37:12 +0700

    initial project

---
+++ .deps/FundMe.sol
@@ -0,0 +1,12 @@
+// get fund from users
+// withdraw
+// set minimum value funding in usd
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe {
+    function fund() public {}
+
+    // function withdraw() public {}
+}

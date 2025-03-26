ae108f4a2ea66efb0a06034995f3ea30120fb774
Lou1sVuong <lou1svuong.dev@gmail.com>
2025-03-26 08:37:50 +0700

    add getConversionRate, getPrice for get and convert realtime price

--- .deps/FundMe.sol
+++ .deps/FundMe.sol
@@ -5,10 +5,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    function fund() public payable  {
-        
+    AggregatorV3Interface internal dataFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: ETH/USD
+     * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+     */
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
     }
+
+    uint256 minimumUsd = 5 * 1e18;
+
+    
+
+    function getPrice() public view returns (uint256) {
+        (
+            ,
+            /* uint80 roundID */
+            int256 answer,
+            ,
+            ,
+
+        ) = /*uint startedAt*/
+            /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            dataFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns (uint256) {
+        return dataFeed.version();
+    }
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd, "You need to send more value");
+    }
+
+    
 
     // function withdraw() public {}
 }
